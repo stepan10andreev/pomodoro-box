0f6b4d9b353ccc803844af102c806add46113db5
Stepan Andreev <stepan10andreev@gmail.com>
2023-05-19 14:47:22 +0300

    add NavLink in Header and isActive-state

--- src/app/components/Header/Logo/Logo.tsx
+++ src/app/components/Header/Logo/Logo.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import styles from './logo.css';
 import { EIcons, Icon } from '../../Icon';
 import { EColor, Text } from '../../Text';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 
 export function Logo() {
   return (
-    <Link to='/'className={styles.logo}>
+    <NavLink to='/'className={({isActive}) => isActive ? styles.linkActive : styles.logo}>
       <Icon name={EIcons.logo} size={40}/>
       <Text As={'h1'} size={24} color={EColor.red} weight={300}>
         pomodoro_box
       </Text>
-    </Link>
+    </NavLink>
   );
 }
--- src/app/components/Header/StatisticsLink/StatisticsLink.tsx
+++ src/app/components/Header/StatisticsLink/StatisticsLink.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import styles from './statisticslink.css';
 import { EIcons, Icon } from '../../Icon';
 import { EColor, Text } from '../../Text';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 
 export function StatisticsLink() {
   return (
-    <Link to='/statistics' className={styles.statisticsLink}>
-      <Icon name={EIcons.equalizer} size={24}/>
+    <NavLink to='/statistics' className={({isActive}) => isActive ? styles.linkActive : styles.statisticsLink}>
+      <Icon name={EIcons.equalizer} />
       <Text size={16} color={EColor.red}>
         Статистика
       </Text>
-    </Link>
+    </NavLink>
   );
 }
 
