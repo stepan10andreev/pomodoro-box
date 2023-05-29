c0618c778770f3377d73575942ee1861c20560df
Stepan Andreev <stepan10andreev@gmail.com>
2023-05-29 13:27:46 +0300

    add ThemeSwitcher

--- src/app/components/Header/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/app/components/Header/ThemeSwitcher/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import styles from './themeswitcher.css';
 import { useAppSelector } from '../../Hooks/useAppDispatch';
 import { useDispatch } from 'react-redux';
 import { setTheme } from '../../../store/theme/theme';
+import classNames from 'classnames';
 
 export function ThemeSwitcher() {
 
@@ -19,10 +20,31 @@ export function ThemeSwitcher() {
     dispatch(setTheme(next))
   }
 
+  // const themeIconClasses = classNames(
+  //   {[styles.lightThemeIcon]: theme === 'light'},
+  //   {[styles.darkThemeIcon]: theme === 'dark'},
+  //   // {[styles.backgroundDF]: indicatorIcons.pause},
+  //   // {[styles.backgroundC5]: indicatorIcons.stop},
+  // )
+
   return (
-    <div
-      className={theme === 'dark' ? styles.root + ' ' +  styles.dark : styles.root + ' ' + styles.light}
+    <label className={styles.root} htmlFor="toggler">
+    <input
+      id="toggler"
+      type="checkbox"
       onClick={handleChange}
+      checked={theme === 'dark'}
+      readOnly
     />
+    <span className={styles.slider} >
+      <span className={styles.lightThemeIcon + `${theme === 'light' ? ' ' + styles.hidden : ''}`}>🌞</span>
+      <span className={styles.darkThemeIcon + `${theme === 'dark' ? ' ' + styles.hidden : ''}`}>🌚</span>
+    </span>
+    <span className={styles.wave} />
+  </label>
+    // <div
+    //   className={theme === 'dark' ? styles.root + ' ' +  styles.dark : styles.root + ' ' + styles.light}
+    //   onClick={handleChange}
+    // />
   );
 }
